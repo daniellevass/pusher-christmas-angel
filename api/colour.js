88f58bd6427b9fb27eb6eadd6a45e892bb53bce9
Danielle Vass <danielle.vass@pusher.com>
2019-12-02 15:25:00 +0000

    initial commit

---
+++ api/colour.js
@@ -0,0 +1,30 @@
+const Pusher = require('pusher')
+
+const pusher = new Pusher({
+  appId: process.env.PUSHER_ANGEL_APP_ID,
+  key: process.env.PUSHER_ANGEL_KEY,
+  secret: process.env.PUSHER_ANGEL_SECRET,
+  cluster:  process.env.PUSHER_ANGEL_CLUSTER,
+  useTLS: true
+})
+
+module.exports = async (req, res) => {
+    let colour = req.body.colour
+
+      try {
+        await new Promise((resolve, reject) => {
+          pusher.trigger
+            ('angel', { colour },
+            err => {
+              if (err) return reject(err)
+              resolve()
+            }
+          )
+        }
+        )
+        console.log(colour)
+        res.status(200).send()
+    } catch(e) {
+        console.log(e.message)
+    }
+}
